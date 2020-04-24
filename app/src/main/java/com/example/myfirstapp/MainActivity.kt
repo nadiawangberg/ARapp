7041b7189acba1587eaf0d3f0a73701e5a5da75e
Nadia <nadiawangberg98@gmail.com>
2020-04-24 16:39:55 +0200

    AR functionality set up

---
+++ app/src/main/java/com/example/myfirstapp/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.example.myfirstapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
+import android.os.Bundle
+import android.provider.AlarmClock.EXTRA_MESSAGE
+import android.view.View
+import android.widget.EditText
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    //CODE!
+    fun sendMessage(view: View) {
+        // Do something in response to button
+        val editText = findViewById<EditText>(R.id.editText)
+        val message = editText.text.toString()
+        val intent = Intent(this, DisplayMessageActivity::class.java).apply {
+            putExtra(EXTRA_MESSAGE, message) // Adds the value of EditText to the intent
+        }
+        startActivity(intent) // starts DisplayMessageActivity specified by Intent
+    }
+
+
+}
